c4ef6cdbc61615cd828b450ef5b76c3d7978202d
FarrellAbiezaZidan-13519182 <arbiterafarra@gmail.com>
2023-02-14 07:10:55 +0700

    little param fix on line
    ada yang masih harus diubah sedikit, rencana seharusnya rotasinya yang dipengaruhi sama current line position, untuk width akan diambil berdasarkan panjang line dan bukan perbedaan kedua koordinat x dari vertex line.

--- src/models/line.js
+++ src/models/line.js
@@ -9,7 +9,18 @@ export class Line extends Model {
         this.name = `Line_${id}`;
     }
 
+    getWidth = () => {
+        return Math.abs(this.vertices[0].coordinate[0] - this.vertices[1].coordinate[0]);
+    }
 
+    getHeight = () => {
+        return Math.abs(this.vertices[0].coordinate[1] - this.vertices[1].coordinate[1]);
+    }
+
+    setWidth = (w) => {
+        const diff = (w - this.getWidth()) / 2;
+        const angle = this.rotation * Math.PI / 180;
+    }
 
     setWidth = (w) => {
         const diff = (w - this.getWidth()) / 2;
@@ -34,9 +45,4 @@ export class Line extends Model {
 
         this.computeCenter();
     }
-
-    //setFirstPoint = (v1) => {
-    //    this.vertices[0].coordinate[0] = diff * Math.cos(this.rotation * Math.PI / 180);
-    //    this.computeCenter();
-    //}
 }
