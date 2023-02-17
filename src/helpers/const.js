4ea3ac8266b0a44671e58acf5c3f6bfd5022ab0f
Claudia <88203931+clauculus@users.noreply.github.com>
2023-02-17 22:13:58 +0700

    fix: change color const

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,12 +1,19 @@
 // create a map containing color and its hex code
 const CMAP = new Map();
-CMAP.set("red", [255,0,0,1]);
-CMAP.set("green",[0,255,0,1]);
-CMAP.set("blue", [0,0,255,1]);
-CMAP.set("yellow", [255,255,0,1]);
-CMAP.set("black", [0,0,0,1]);
+CMAP.set("red", [1, 0, 0, 1]);
+CMAP.set("green", [0, 1, 0, 1]);
+CMAP.set("blue", [0, 0, 1, 1]);
+CMAP.set("yellow", [1, 1, 0, 1]);
+CMAP.set("black", [0, 0, 0, 1]);
+
+const RCMAP = new Map();
+RCMAP.set([1, 0, 0, 1].toString(), "red");
+RCMAP.set([0, 1, 0, 1].toString(), "green");
+RCMAP.set([0, 0, 1, 1].toString(), "blue");
+RCMAP.set([1, 1, 0, 1].toString(), "yellow");
+RCMAP.set([0, 0, 0, 1].toString(), "black");
 
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
 
-export { CMAP, CANVAS_HEIGHT, CANVAS_WIDTH };
+export { CMAP, RCMAP, CANVAS_HEIGHT, CANVAS_WIDTH };
